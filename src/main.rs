fb28bbdda2f1e0d13df27449f4740720d5ab2258
Fonkin Stubbleduck <fonkinstubbleduck@gmail.com>
2025-02-22 13:25:38 +0100

    Added support for HTML page titling, and adding own shell/wrap in html idk what to call it

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,10 @@ struct Cli {
     // Flag for whether uft-8 should be allowed
     #[arg(long)]
     utf8: bool,
-    // Flag for whether html structure should be included
     #[arg(long)]
-    head: bool,
+    custombody: Option<std::path::PathBuf>,
+    #[arg(short, long)]
+    title: Option<String>,
 }
 
 fn main() {
@@ -54,20 +55,42 @@ fn main() {
     //let (line_types, line_tokens) = lexer::analyze(&lines);
 
     let mut html = String::new();
-    if args.head {
-        html.push_str("<!DOCTYPE html><html><body>");
-    }
-    if args.utf8 {
-        html.push_str("<meta charset=\"UTF-8\">");
-    }
-    if args.css_path.is_some() {
-        html.push_str("<link rel=\"stylesheet\" href=\"");
-        html.push_str(args.css_path.unwrap().to_str().unwrap());
-        html.push_str("\">");
+    let mut include_head = false;
+    let mut custombody_end = String::new();
+    if args.title.is_some() || args.utf8 || args.css_path.is_some() || args.custombody.is_some() {
+        include_head = true;
+        html.push_str("<!DOCTYPE html><html><head>");
+        if args.title.is_some() {
+            html.push_str("<title>");
+            html.push_str(&args.title.unwrap());
+            html.push_str("</title>");
+        }
+        if args.utf8 {
+            html.push_str("<meta charset=\"UTF-8\">");
+        }
+        if args.css_path.is_some() {
+            html.push_str("<link rel=\"stylesheet\" href=\"");
+            html.push_str(args.css_path.unwrap().to_str().unwrap());
+            html.push_str("\">");
+        }
+        if args.custombody.is_some() {
+            let p = fs::read_to_string(args.custombody.unwrap());
+            if let Err(e) = p {
+                return handle_error(e.to_string());
+            }
+            let p = p.expect("not error");
+            let p = p.split("\n").collect::<Vec<&str>>();
+            html.push_str(p[0]);
+            custombody_end = p[1].to_string();
+        }
+        html.push_str("</head><body>");
     }
 
     html.push_str(&htmloutput::convert(tokens));
-    if args.head {
+    if include_head {
+        if custombody_end.len() != 0 {
+            html.push_str(&custombody_end);
+        }
         html.push_str("</body></html>");
     }
 
